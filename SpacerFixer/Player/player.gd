4047559d02a77c2b03973d14f7ee9098e5956fe5
Alex Zimmer <alex.zimmer@nimblegiant.com>
2022-03-10 17:35:00 -0300

    Player assets and controller player

---
+++ SpacerFixer/Player/player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody
+
+const MAXSPEED = 30
+const ACCELERATION = 0.75
+var inputVector = Vector3()
+var velo = Vector3()
+
+func _physics_process(delta):
+	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+	inputVector = inputVector.normalized()
+	velo.x = move_toward(velo.x, inputVector.x * MAXSPEED, ACCELERATION)
+	velo.y = move_toward(velo.y, inputVector.y * MAXSPEED, ACCELERATION)
+	rotation_degrees.z = velo.x * -2
+	rotation_degrees.x = velo.y / 2
+	rotation_degrees.y = -velo.x / 2
+	move_and_slide(velo)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
