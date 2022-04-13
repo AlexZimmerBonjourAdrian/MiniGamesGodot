50e7a33d1a54a479839bf7a11729641e18913d9f
Alex Zimmer <alexzimmer.zimmer1@gmail.com>
2022-04-13 17:16:21 -0300

    Animation Sprite Controller

--- Draconiano_leporidae_vertical_slide/LevelTest/levels/SpriteAnimationController.gd
+++ Draconiano_leporidae_vertical_slide/LevelTest/levels/SpriteAnimationController.gd
@@ -8,43 +8,56 @@ onready var ppk_Reload = $CanvasLayer/Control/PPK/Reload
 onready var ppk_Idle = $CanvasLayer/Control/PPK/Idle
 onready var animation = $AnimationPlayer
 
-var is_crossing = false
+var is_crossing 
+
 func _ready():
 		ppk_Shoot.visible = false
 		ppk_Reload.visible = false
 		ppk_Crosshai.visible = false
 		ppk_Idle.visible = true
+		is_crossing= false
 		animation.play("iDLE")
 		
-
-func _input(event):
-	if event.is_action_pressed("click") and !is_crossing:
-		ppk_Idle.visible = false
-		ppk_Reload.visible = false
-		ppk_Shoot.visible = true
-		animation.play("shoot")
-		
-	elif event.is_action_pressed("reload") and !is_crossing:
-		 ppk_Idle.visible = false
-		 ppk_Shoot.visible = false
-		 ppk_Reload.visible = true
-		 
-		 animation.play("Reload")
-
+func _input(event):	
 #	Crosshair
-	elif event.is_action_pressed("Iron_Crosshair"):
+	if event.is_action_pressed("Iron_Crosshair"):
 		ppk_Idle.visible = false
 		ppk_Reload.visible = false
 		ppk_Shoot.visible = false
 		ppk_Crosshai.visible = true
-		is_crossing= true
+		setIsCrossing(true)
 		animation.play("Crosshair")
-	elif event.is_action_released("Iron_Crosshair"):
-		ppk_Idle.visible = false
-		ppk_Shoot.visible = false
-		ppk_Reload.visible = false
+		
+	if event.is_action_released("Iron_Crosshair"):
+#		ppk_Idle.visible = false
+#		ppk_Shoot.visible = false
+#		ppk_Reload.visible = false
 		animation.play_backwards("Crosshair")
-#		_on_AnimationPlayer_animation_finished("Crosshair")
+		if(animation.get_animation("Crosshair") and animation.is_playing()):
+			setIsCrossing(false)
+#		if(_on_AnimationPlayer_animation_finished("Crosshair")):
+#			setIsCrossing(false)
+			
+	elif event.is_action_pressed("click"):
+		if (is_crossing==false):
+			ppk_Idle.visible = false
+			ppk_Crosshai.visible = false
+			ppk_Reload.visible = false
+			ppk_Shoot.visible = true
+			animation.play("shoot")
+		
+	elif event.is_action_pressed("reload"):
+		if(is_crossing==false):
+			 ppk_Idle.visible = false
+			 ppk_Crosshai.visible = false
+			 ppk_Shoot.visible = false
+			 ppk_Reload.visible = true
+			 animation.play("Reload")
+#		if(animation.is_playing() == false):
+#				setIsCrossing(false)
+#				ppk_Idle.visible = true
+#				animation.play("iDLE")
+		
 #	else:
 #		ppk_Reload.visible = false
 #		ppk_Shoot.visible = false
@@ -52,13 +65,16 @@ func _input(event):
 #		ppk_Crosshai.visible = false
 #		ppk_Idle.visible = true
 #		animation.play("iDLE")
-	
-	
+#	print(is_crossing)
+	update_Print(is_crossing)
 
-func _on_AnimationPlayer_animation_finished(anim_name):
-	ppk_Reload.visible = false
-	ppk_Shoot.visible = false
-	is_crossing= false
-	ppk_Crosshai.visible = false
-	ppk_Idle.visible = true
-	animation.play("iDLE")
+#	ppk_Reload.visible = false
+#	ppk_Shoot.visible = false
+
+#	ppk_Crosshai.visible = false
+
+func setIsCrossing(Crossing):
+	is_crossing = Crossing
+
+func update_Print(testprinter):
+	$Control/Background/Print.text = String(testprinter)
